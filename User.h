a62d2e8539fefe522609df7268b5d1d8f41b1e0b
SreejaPagireddy <117553441+SreejaPagireddy@users.noreply.github.com>
2023-11-17 15:32:53 -0800

    Update User.h

--- User.h
+++ User.h
@@ -1,4 +1,14 @@
-
 using namespace std;
 class User{
+    private:
+        string userName;
+        string password;
+    public:
+        User::User();
+        User::User(string _userName, string _password);
+        void SetUserName (string _userName);
+        void SetPassword (string _password);
+        string GetUserName();
+        string GetPassword();
+        void loadtasks();
 };
