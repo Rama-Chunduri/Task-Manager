39b678d8bd03524de0902b048171b88424d4439a
Varsha Kuppa <vkupp001@xe-01.cs.ucr.edu>
2023-11-17 16:36:18 -0800

    Started writing function definition for the view Schedule class

--- viewSchedule.cpp
+++ viewSchedule.cpp
@@ -3,6 +3,39 @@
 
 using namespace std;
 
-void viewSchedule :: printByDate() {
-    
+// earliest to latest start date
+void viewSchedule :: printByStartDate(User* user)  
+{
+
+}
+
+// earliest to latest start date 
+// also the default view 
+void viewSchedule :: printByDueDate(User* user) 
+{ 
+
+}
+
+// longest to shortest duration
+void viewSchedule :: printByDuration(User* user)  
+{
+
+}
+
+// complete vs incomplete
+void viewSchedule :: printByCompletion(User* user)  
+{
+
+}
+
+// groups tasks of the same tag together
+void viewSchedule :: printByTag(User* user)  
+{
+
+}
+
+// highest(1) to lowest priority(3)
+void viewSchedule :: printByPriority(User* user) 
+{
+
 }
--- viewSchedule.h
+++ viewSchedule.h
@@ -5,10 +5,24 @@
 using namespace std;
 
 class ViewSchedule {
+    
+    // earliest to latest start date
     void printByStartDate(User* user);
+    
+    // earliest to latest start date 
+    // also the default view 
     void printByDueDate(User* user);
+    
+    // longest to shortest duration
     void printByDuration(User* user);
+    
+    // complete vs incomplete
     void printByCompletion(User* user);
+    
+    // groups tasks of the same tag together
     void printByTag(User* user);
+
+    // highest(1) to lowest priority(3)
     void printByPriority(User* user);
+
 }
