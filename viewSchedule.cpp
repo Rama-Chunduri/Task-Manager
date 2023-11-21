315efb4888e31ba66011bf08898dcffa918b1f12
Varsha Kuppa <vkupp001@xe-01.cs.ucr.edu>
2023-11-21 15:49:42 -0800

    Changed the functions

--- viewSchedule.cpp
+++ viewSchedule.cpp
@@ -2,35 +2,35 @@
 
 using namespace std;
 
-// earliest to latest start date
-void viewSchedule :: printByStartDate(User* user)  
-{
-    //vector<Task> tasks = user->GetTaskList().getTasks();
-}
+// // earliest to latest start date
+// void viewSchedule :: printByStartDate(User* user)  
+// {
+//     //vector<Task> tasks = user->GetTaskList().getTasks();
+// }
 
-// earliest to latest start date 
-// also the default view 
-void viewSchedule :: printByDueDate(User* user) 
-{ 
-    //vector<Task> tasks = user->GetTaskList().getTasks();
+// // earliest to latest start date 
+// // also the default view 
+// void viewSchedule :: printByDueDate(User* user) 
+// { 
+//     //vector<Task> tasks = user->GetTaskList().getTasks();
 
-}
+// }
 
-// longest to shortest duration
-void viewSchedule :: printByDuration(User* user)  
-{
-    vector<Task> tasks = user->GetTaskList().getTasks();
+// // longest to shortest duration
+// void viewSchedule :: printByDuration(User* user)  
+// {
+//     vector<Task> tasks = user->GetTaskList().getTasks();
 
-    sort(tasks.begin(), tasks.end())
+//     sort(tasks.begin(), tasks.end())
 
-}
+// }
 
-// complete vs incomplete
-void viewSchedule :: printByCompletion(User* user)  
-{
-    vector<Task> tasks = user->GetTaskList().getTasks();
+// // complete vs incomplete
+// void viewSchedule :: printByCompletion(User* user)  
+// {
+//     vector<Task> tasks = user->GetTaskList().getTasks();
 
-}
+// }
 
 // groups tasks of the same tag together
 void viewSchedule :: printByTag(User* user)  
@@ -39,9 +39,9 @@ void viewSchedule :: printByTag(User* user)
 
 }
 
-// highest(1) to lowest priority(3)
-void viewSchedule :: printByPriority(User* user) 
-{
-    vector<Task> tasks = user->GetTaskList().getTasks();
+// // highest(1) to lowest priority(3)
+// void viewSchedule :: printByPriority(User* user) 
+// {
+//     vector<Task> tasks = user->GetTaskList().getTasks();
 
-}
+// }
