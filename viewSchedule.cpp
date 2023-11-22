24e356dea72119f5828a75f71bc3b2356851b379
Varsha Kuppa <vkupp001@xe-01.cs.ucr.edu>
2023-11-21 22:24:26 -0800

    Finished viewByTag function

--- viewSchedule.cpp
+++ viewSchedule.cpp
@@ -35,7 +35,36 @@ using namespace std;
 // groups tasks of the same tag together
 void viewSchedule :: printByTag(User* user)  
 {
-    vector<Task> tasks = user->GetTaskList().getTasks();
+    vector<Task> tasks = user->getTaskList().getTasks();
+
+    //get unique tags 
+    vector<string>uniqueTags;
+    Task taskIterator = tasks.at(0);
+
+    for (int i = 0; i < tasks.size(); ++i) {
+    
+        // if tag is not found in uniqueTags
+        if (find(uniqueTags.begin(), uniqueTags.end(), iterator.getTag()) == uniqueTags.end()) {
+            uniqueTags.push_back(task.getTag());
+        }
+    }
+
+    //iterate over unique tags and print tasks for each tag 
+    string tagIterator = uniqueTag.at(0);
+    taskIterator - tasks.at(0);
+
+    for (int i = 0; i < uniqueTag.size(); ++i) {
+        cout << "+--------------------- " << tagIterator << " ---------------------+" << endl;
+
+        //iterate over tasks and print details for tasks with the current tag
+        for (int i = 0; i < uniqueTasks.size(); ++i) {
+            if (task.getTag() == tagIterator)
+                //print task details as needed 
+                cout << "| " << task.getDueDate() << " - " << task.getName() << endl;
+        }
+    }
+
+    cout << "+---------------------------------------------------+" << endl;
 
 }
 
