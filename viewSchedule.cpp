25804128fd392e754c0ff4dd9fc6dc97479c9498
Varsha Kuppa <vkupp001@xe-01.cs.ucr.edu>
2023-11-29 22:47:13 -0800

    Grouping by tag function done

--- viewSchedule.cpp
+++ viewSchedule.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 
 void viewSchedule :: viewSchedule(User * user) {
-
     
+
 }
 
 
@@ -16,35 +16,6 @@ void viewSchedule :: viewSchedule(User * user) {
 
 
 
-// // earliest to latest start date
-// void viewSchedule :: printByStartDate(User* user)  
-// {
-//     //vector<Task> tasks = user->GetTaskList().getTasks();
-// }
-
-// // earliest to latest start date 
-// // also the default view 
-// void viewSchedule :: printByDueDate(User* user) 
-// { 
-//     //vector<Task> tasks = user->GetTaskList().getTasks();
-
-// }
-
-// // longest to shortest duration
-// void viewSchedule :: printByDuration(User* user)  
-// {
-//     vector<Task> tasks = user->GetTaskList().getTasks();
-
-//     sort(tasks.begin(), tasks.end())
-
-// }
-
-// // complete vs incomplete
-// void viewSchedule :: printByCompletion(User* user)  
-// {
-//     vector<Task> tasks = user->GetTaskList().getTasks();
-
-// }
 
 // groups tasks of the same tag together
 void viewSchedule :: printByTag(User* user)  
@@ -82,9 +53,3 @@ void viewSchedule :: printByTag(User* user)
 
 }
 
-// // highest(1) to lowest priority(3)
-// void viewSchedule :: printByPriority(User* user) 
-// {
-//     vector<Task> tasks = user->GetTaskList().getTasks();
-
-// }
